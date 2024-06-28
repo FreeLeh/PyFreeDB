d6d88d86338839a09d6dc84c4c0eefcd61505387
Edwin Candinegara <edwin.candinegara@gmail.com>
2024-06-28 11:30:02 +0700

    Fix some linting issues

--- src/pyfreedb/providers/google/auth/base.py
+++ src/pyfreedb/providers/google/auth/base.py
@@ -1,11 +1,12 @@
 import abc
+from typing import Union
 
-from google.oauth2.credentials import Credentials
+from google.oauth2 import credentials, service_account
 
 
 class GoogleAuthClient(abc.ABC):
     """An abstraction layer that represents way to authenticate with Google APIs."""
 
     @abc.abstractmethod
-    def credentials(self) -> Credentials:
+    def credentials(self) -> Union[credentials.Credentials, service_account.Credentials]:
         pass
